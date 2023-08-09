a5f1ee11988d4e9c5660255698fce77eecee4f65
fbwer <dlawoqja123@gmail.com>
2023-08-10 02:59:05 +0900

    0810-2

--- crawl-ref/source/god-passive.h
+++ crawl-ref/source/god-passive.h
@@ -274,7 +274,7 @@ bool will_have_passive(passive_t passive);
 int rank_for_passive(passive_t passive);
 int chei_stat_boost(int piety = you.piety);
 void jiyva_eat_offlevel_items();
-int ash_scry();
+void ash_scry();
 void ash_check_bondage();
 bool god_id_item(item_def& item, bool silent = true);
 int ash_detect_portals(bool all);
